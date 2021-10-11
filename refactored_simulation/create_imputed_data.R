852235278dde0d3de52f301f62c941c55c6f138d
Tristan Trouwen <tristan.trouwen@gmail.com>
2021-10-11 17:52:41 +0200

    Added imputation code

--- refactored_simulation/create_imputed_data.R
+++ refactored_simulation/create_imputed_data.R
@@ -3,7 +3,7 @@
 ##
 
 # install and import all packages
-neededPackages <- c("dplyr", "mice", "missForest")
+neededPackages <- c("dplyr", "mice", "missForest", "farff")
 installedPackages <- neededPackages %in% installed.packages()
 if (length(neededPackages[!installedPackages]) > 0) {
   install.packages(neededPackages[!installedPackages], repos=c("http://rstudio.org/_packages", "http://cran.rstudio.com"))
@@ -11,5 +11,19 @@ if (length(neededPackages[!installedPackages]) > 0) {
 lapply(neededPackages, require, character.only=TRUE)
 
 source("helper_functions.R")
+source("imputation_methods.R")
+
+# iterate over all datasets
+datasets <- list.files(file.path("datasets", "original"), pattern="*.arff")
+for (dataset in datasets) {
+  dataframe <- readARFF(file.path("datasets", "original", dataset))
+  
+  # start imputation
+  dataframe <- missingForest(dataframe)
+  # save files
+  filename <- paste("missingForest", dataframe, sep="_")
+  writeARFF(dataframe, file.path("datasets", "imputed", filename), overwrite=TRUE)
+}
+
 
 # TODO: finish file
---
+++ refactored_simulation/imputation_methods.R
@@ -0,0 +1,28 @@
+##
+# File containing the imputation method functions.
+##
+
+missingForest <- function(dataframe) {
+  imputedData <- missForest(dataframe)
+  return (imputedData)
+}
+
+listwiseDeletion <- function(dataframe) {
+  # TODO: implement
+  return (dataframe)
+}
+
+modeImputation <- function(dataframe) {
+  # TODO: implement
+  return (dataframe)
+}
+
+kNeirestImputation <- function(dataframe) {
+  # TODO: implement
+  return (dataframe)
+}
+
+miceImputation <- function(dataframe) {
+  # TODO: implement
+  return (dataframe)
+}
