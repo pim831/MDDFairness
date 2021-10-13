73ac55a8bd040a5034fc2f00cc0fe66585524988
Tristan Trouwen <tristan.trouwen@gmail.com>
2021-10-13 09:49:04 +0200

    Created start with training and testing code

--- refactored_simulation/create_classification_results.R
+++ refactored_simulation/create_classification_results.R
@@ -3,7 +3,7 @@
 ##
 
 # install and import all packages
-neededPackages <- c("dplyr")
+neededPackages <- c("caret", "dplyr", "farff", "nnet")
 installedPackages <- neededPackages %in% installed.packages()
 if (length(neededPackages[!installedPackages]) > 0) {
   install.packages(neededPackages[!installedPackages], repos=c("http://rstudio.org/_packages", "http://cran.rstudio.com"))
@@ -13,4 +13,50 @@ lapply(neededPackages, require, character.only=TRUE)
 source("helper_functions.R")
 
 
-# TODO: create file
+##
+# Returns multinomial logistic regression model trained on data.
+##
+runModel <- function(data) {
+  x <- dataTrain[, ncol(data)]
+  y <- dataTrain[, -ncol(data)]
+  
+  # TODO: do multinomial logistic regression
+  
+  #multinomModel <- multinomial(paste("class", " ~ .", sep=""), d)
+  
+}
+
+
+##
+# Returns predictions of data based on the given trained model.
+##
+testData <-  function(model, data) {
+  # TODO: implement
+}
+
+
+datasets <- list.files(file.path("datasets", "original"), pattern="*.arff")  # read original file
+
+for (dataset in datasets) {
+  dataframe <- readARFF(file.path("datasets", "original", dataset))
+  index <- createDataPartition(data, p = 0.75, list=FALSE)
+
+  imputationMethods <- c("ld", "knn", "modeImputation", "missingForestImputation", "mice")
+  
+  for (imputationMethod in imputationMethods) {
+    datasetName <- paste(imputationMethod, dataset, sep = "_")
+    dataframe <- readARFF(file.path("datasets", "imputed", datasetName))
+    trainData <- data[index,]
+    testData <- data[-index,]  
+    
+    # train model
+    trainedModel <- runModel(trainData)
+    
+    # create predictions for test dataset
+    preds <- testData(trainedModel, testData)
+    
+    # TODO: compute and save accuracy/spd of result on test
+  }
+  
+
+}
