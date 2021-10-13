19832e5aed60ef91f3239701d59a77a77f79f86a
leightonvg <leightonvangellecom@gmail.com>
2021-10-13 11:52:24 +0200

    added mice imputation

--- refactored_simulation/imputation_methods.R
+++ refactored_simulation/imputation_methods.R
@@ -23,6 +23,14 @@ kNeirestImputation <- function(dataframe) {
 }
 
 miceImputation <- function(dataframe) {
-  # TODO: implement
+  # if c_charge_desc in dataframe do not consider the feature in imputation
+  if ("c_charge_desc" %in% colnames(dataframe)){
+    data_dropped <- subset(dataframe, select = -c(c_charge_desc)) # without c_charge_desc
+    imp_data_dropped <- complete(mice(dataframe, m = 1), action=1)
+    imp_data <- cbind(imp_data_dropped, dataframe$c_charge_desc) #add c_charge_desc back in
+    dataframe <- na.omit(imp_data) #omit rows with missing values (so only for c_charge_desc)
+  } else {
+    dataframe <- complete(mice(dataframe, m = 1, nnet.MaxNWts = 4000), action=1) 
+  }
   return (dataframe)
 }
