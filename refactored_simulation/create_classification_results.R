7a897cedf8504a203e7ab69da63dff5d1827c718
Tristan Trouwen <tristan.trouwen@gmail.com>
2021-10-13 10:25:04 +0200

    Finished regression model and fixed several small bugs in code.

--- refactored_simulation/create_classification_results.R
+++ refactored_simulation/create_classification_results.R
@@ -17,16 +17,26 @@ source("helper_functions.R")
 # -------------------------------------------------------------------------------------------------
 
 ##
+# Preprocess data. 
+#
+# Specifically, mark class as factor for regression model.
+##
+preprocessData <- function(data) {
+  
+  data$class <- as.factor(data$class)
+  return (data)
+}
+
+##
 # Returns multinomial logistic regression model trained on data.
 ##
-runModel <- function(data) {
-  x <- dataTrain[, ncol(data)]
-  y <- dataTrain[, -ncol(data)]
+trainModel <- function(data) {
+  # create logistic regression model (https://stats.idre.ucla.edu/r/dae/logit-regression/)
+  ob <- as.formula(paste("class ~ .", sep=""))
+  train_control <- trainControl(method = "none", classProbs = FALSE)
+  model <- train(ob, data=data, method="glm", trControl=train_control, family="binomial", na.action=na.pass)
   
-  # TODO: do multinomial logistic regression
-  
-  #multinomModel <- multinomial(paste("class", " ~ .", sep=""), d)
-  
+  return (model)
 }
 
 
@@ -34,7 +44,8 @@ runModel <- function(data) {
 # Returns predictions of data based on the given trained model.
 ##
 testData <-  function(model, data) {
-  # TODO: implement
+  preds <- predict(model, newdata=data)
+  return (preds)
 }
 
 
@@ -46,18 +57,19 @@ datasets <- list.files(file.path("datasets", "original"), pattern="*.arff")  # r
 
 for (dataset in datasets) {
   dataframe <- readARFF(file.path("datasets", "original", dataset))
-  index <- createDataPartition(data, p = 0.75, list=FALSE)
+  index <- createDataPartition(dataframe, p = 0.75, list=FALSE) # split data in 75% train, 25% test
 
   imputationMethods <- c("ld", "knn", "modeImputation", "missingForestImputation", "mice")
   
   for (imputationMethod in imputationMethods) {
     datasetName <- paste(imputationMethod, dataset, sep = "_")
     dataframe <- readARFF(file.path("datasets", "imputed", datasetName))
-    trainData <- data[index,]
-    testData <- data[-index,]  
+    dataframe <- preprocessData(dataframe)
+    trainData <- dataframe[index,]
+    testData <- dataframe[-index,]  
     
     # train model
-    trainedModel <- runModel(trainData)
+    trainedModel <- trainModel(trainData)
     
     # create predictions for test dataset
     preds <- testData(trainedModel, testData)
