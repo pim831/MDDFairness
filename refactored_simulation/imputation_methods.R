5e6d01c92e6f4e0906685cde61bdb6f07740949a
hotStuffz <leightonvangellecom@gmail.com>
2021-10-14 14:50:46 +0200

    adapted such that original name is present for non-considered variables in MICE

--- refactored_simulation/imputation_methods.R
+++ refactored_simulation/imputation_methods.R
@@ -26,8 +26,8 @@ miceImputation <- function(dataframe) {
   # if c_charge_desc in dataframe do not consider the feature in imputation
   if ("c_charge_desc" %in% colnames(dataframe)){
     data_dropped <- subset(dataframe, select = -c(c_charge_desc)) # without c_charge_desc
-    imp_data_dropped <- complete(mice(dataframe, m = 1), action=1)
-    imp_data <- cbind(imp_data_dropped, dataframe$c_charge_desc) #add c_charge_desc back in
+    imp_data_dropped <- complete(mice(data_dropped, m = 1), action=1)
+    imp_data <- cbind(imp_data_dropped, c_charge_desc = dataframe$c_charge_desc) #add c_charge_desc back in
     dataframe <- na.omit(imp_data) #omit rows with missing values (so only for c_charge_desc)
   } else {
     dataframe <- complete(mice(dataframe, m = 1, nnet.MaxNWts = 4000), action=1) 
