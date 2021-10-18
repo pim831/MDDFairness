9b495cfe762cfec6362346d93571957b8a5abd0e
Tristan Trouwen <tristan.trouwen@gmail.com>
2021-10-18 15:10:50 +0200

    Add helper function mode

--- refactored_simulation/helper_functions.R
+++ refactored_simulation/helper_functions.R
@@ -1,3 +1,17 @@
 ##
 # File containing helper functions.
 ##
+
+##
+# Statistical Parity Difference (SPD) [-1,1]:
+# (TPP+FPP)/(NP)-(TPU+FPU)/NU
+##
+compute_spd <- function(cmp, cmnop) {
+  return (((cmp$TP+cmp$FP)/cmp$N) - ((cmnop$TP+cmnop$FP)/cmnop$N))
+}
+
+# mode function
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
